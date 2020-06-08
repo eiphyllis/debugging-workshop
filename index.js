95d2e75c757042ffa3b360d04d3bc37c5f67a243
eiphyllis <phyllisgetcity@gmail.com>
2020-06-07 21:07:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
@@ -12,16 +9,28 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+    joke = jokeData.joke
+    printJoke(username, joke)
+  })
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('name-input').value
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
+    fetchJoke(username)
+    
+  })
+})
+
+function printJoke(username, joke){
+  const jokeList = document.getElementById('joke-list')
+  const newJokeLi = document.createElement('li')
+
+  newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
     jokeList.appendChild(newJokeLi)
-  })
-})
+}
